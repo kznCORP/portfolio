c79f48968319b3afcf5d8f45725484bb42e0cfa5
Jericho FIllon <jerichofilloncoding@gmail.com>
2023-03-28 01:04:28 -0700

    Add OpenGraph image for sharing links

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -9,6 +9,17 @@ const Meta = ({ title, keywords, description }) => {
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
 
+      <meta
+        property="og:title"
+        content="Jericho Fillon | Web Developer & Designer based in Vancouver, B.C."
+      />
+      <meta property="og:type" content="webpage" />
+      <meta property="og:url" content="https://jerichofillon.com/" />
+      <meta
+        property="og:image"
+        content="https://ik.imagekit.io/ricosuave/meta.png?updatedAt=1679990449480"
+      />
+
       <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
 
       <title>{title}</title>
@@ -17,9 +28,10 @@ const Meta = ({ title, keywords, description }) => {
 };
 
 Meta.defaultProps = {
-  title: 'Jericho Fillon | Web Developer',
-  keywords: 'web development, web developer, web design, UI/UX designer',
-  description: 'A multidisciplinary Designer + Developer focusing on helping brands, products and people make a mark.'
-}
+  title: "Jericho Fillon | Web Developer & Designer based in Vancouver, B.C.",
+  keywords: "web development, web developer, web design, UI/UX designer",
+  description:
+    "A multidisciplinary Designer + Developer focusing on helping brands, products and people stand out in today's competitive market.",
+};
 
 export default Meta;
